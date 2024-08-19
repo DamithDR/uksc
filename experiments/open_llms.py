b86523b5ceeeb4332b9c01a0bcd012c069b5ca52
Damith Premasiri <damithpremasiri@gmail.com>
2024-08-19 16:59:35 +0100

    test

--- experiments/open_llms.py
+++ experiments/open_llms.py
@@ -59,6 +59,7 @@ def run(args):
         num_return_sequences=1,
         batch_size=8
     )
+    print(outputs)
     for output in outputs:
         resp = output["generated_text"][-1]['content'].strip()
         decisions.append(resp)
