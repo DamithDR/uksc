f88122d9843d06caed550df5bac9d91670ed6f90
Damith Premasiri <damithpremasiri@gmail.com>
2024-08-19 17:28:58 +0100

    changes for batching and multi prompting

--- experiments/open_llms.py
+++ experiments/open_llms.py
@@ -1,22 +1,53 @@
 import argparse
 import os
-import sys
 
 import pandas as pd
 import torch
 from tqdm import tqdm
-from transformers import pipeline, AutoConfig, AutoTokenizer
+from transformers import pipeline, AutoTokenizer
 
 from util.eval import eval_decisions
 
 
-def run(args):
-    os.environ["CUDA_VISIBLE_DEVICES"] = args.visible_cuda_devices  # set the devices you need to run
-    df = pd.read_excel('data/UKSC_dataset.xlsx', sheet_name='data')
+def get_messages_for_labels(df):
+    label_classification_messages = []
+    for background, decision, reason in tqdm(zip(df['background'], df['decision'], df['reasoning']), total=len(df),
+                                             desc="generating label outputs"):
+        messages = [
+            {"role": "system",
+             "content": "Assume you are a judge at the supreme court in United Kingdom. "
+                        "You will be provided UK supreme court appeal cases by the users and your duty is to understand the case background and output your decision and the reasoning behind it."
+                        "First, classify whether the case is allowed or dismissed, select one from following : [allow,dismiss]"
+             },
+            {"role": "user",
+             "content": f"Following is the case background, please provide the classification label, do not respond anything else other than allow/dismiss. case: {background}"},
+        ]
+        label_classification_messages.append(messages)
+    return label_classification_messages
 
-    df = df[:1]
 
-    tokenizer_mt = AutoTokenizer.from_pretrained(args.model_name)
+def get_messages_for_reasoning(df, decision_labels):
+    reasoning_messages = []
+    for background, decision, reason, label in tqdm(
+            zip(df['background'], df['decision'], df['reasoning'], decision_labels), total=len(df),
+            desc="generating label outputs"):
+        messages = [
+            {"role": "system",
+             "content": "Assume you are a judge at the supreme court in United Kingdom. "
+                        "You will be provided UK supreme court appeal cases by the users and your duty is to understand the case background and output your decision and the reasoning behind it."
+                        "First, classify whether the case is allowed or dismissed, select one from following : [allow,dismiss]"
+             },
+            {"role": "user",
+             "content": f"Following is the case background, please provide the classification label, do not respond anything else other than allow/dismiss. case: {background}"},
+            {"role": "assistant", "content": label},
+            {"role": "user",
+             "content": "Now please provide the reason behind your decision. Carefully consider the case background and your decided label and output the reasoning behind your decision."}
+        ]
+        reasoning_messages.append(messages)
+    return reasoning_messages
+
+
+def get_chat_template():
     chat_template = None
     if str(args.model_name).__contains__('mistral'):
         chat_template = open('templates/mistral-instruct.jinja').read()
@@ -24,10 +55,22 @@ def run(args):
         chat_template = open('templates/falcon-instruct.jinja').read()
     elif str(args.model_name).__contains__('Llama-2'):
         chat_template = open('templates/llama-2-chat.jinja').read()
+    return chat_template
+
+
+def run(args):
+    model_name = str(args.model_name).replace('/', '_')
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.visible_cuda_devices  # set the devices you need to run
+    df = pd.read_excel('data/UKSC_dataset.xlsx', sheet_name='data')
+
+    df = df[:40]
+
+    tokenizer_mt = AutoTokenizer.from_pretrained(args.model_name)
+    chat_template = get_chat_template()
     if chat_template:
         tokenizer_mt.chat_template = chat_template
-    decisions = []
-    reasons = []
+
+    decision_labels = []
     pipe = pipeline(
         "text-generation",
         model=args.model_name,
@@ -38,53 +81,50 @@ def run(args):
     pipe.tokenizer.pad_token_id = pipe.tokenizer.eos_token_id
     pipe.tokenizer.padding_side = 'left'
 
-    all_messages = []
-    for background, decision, reason in tqdm(zip(df['background'], df['decision'], df['reasoning']), total=len(df)):
-        messages = [
-            {"role": "system",
-             "content": "Assume you are a judge at the supreme court in United Kingdom. "
-                        "You will be provided UK supreme court appeal cases by the users and your duty is to understand the case background and output your decision and the reasoning behind it."
-                        "First, classify whether the case is allowed or dismissed, select one from following : [allow,dismiss]"
-             },
-            {"role": "user",
-             "content": f"Following is the case background, please provide the classification label, do not respond anything else other than allow/dismiss. case: {background}"},
-        ]
-        all_messages.append(messages)
-
-    outputs = pipe(
-        all_messages,
+    label_classification_messages = get_messages_for_labels(df)
+    decision_outputs = pipe(
+        label_classification_messages,
         max_new_tokens=2048,
         temperature=0.1,
         pad_token_id=pipe.model.config.eos_token_id,
         num_return_sequences=1,
         batch_size=8
     )
-    for output in outputs:
+    for output in tqdm(decision_outputs, total=len(decision_outputs), desc="extracting label outputs"):
         resp = output[0]["generated_text"][-1]['content'].lower().strip()
-        decisions.append(resp)
-        print(resp)
-
-    sys.exit(0)
-    # df['outputs'] = output_list
-    model_name = str(args.model_name).replace('/', '_')
-    # df.to_csv(f'outputs/output_{model_name}.tsv', sep='\t', index=False)
+        decision_labels.append(resp)
 
     decisions_df = pd.DataFrame()
     decisions_df['gold'] = df['decision_label']
-    decisions_df['predictions'] = decisions
+    decisions_df['predictions'] = decision_labels
+    decisions_df.to_excel(f'outputs/decisions.xlsx', sheet_name=f"{model_name}", index=False)
+
+    # save results of label evaluation
+    w_recall, w_precision, w_f1, m_f1 = eval_decisions(decisions_df, 'predictions', 'gold')
+    with open(f'decision_stats.tsv', 'a') as f:
+        f.write(
+            f'{model_name}\t{round(w_recall, 2)}\t{round(w_precision, 2)}\t{round(w_f1, 2)}\t{round(m_f1, 2)}\n')
+
+    reasoning_messages = get_messages_for_reasoning(df, decision_labels)
+    reasoning_outputs = pipe(
+        reasoning_messages,
+        max_new_tokens=2048,
+        temperature=0.1,
+        pad_token_id=pipe.model.config.eos_token_id,
+        num_return_sequences=1,
+        batch_size=args.batch_size
+    )
+
+    reasons = []
+    for output in tqdm(reasoning_outputs, total=len(reasoning_outputs), desc="extracting reasoning outputs"):
+        resp = output[0]["generated_text"][-1]['content'].strip()
+        reasons.append(resp)
 
     reasons_df = pd.DataFrame()
     reasons_df['gold'] = df['reasoning']
     reasons_df['predictions'] = reasons
 
-    decisions_df.to_excel(f'outputs/{model_name}_decisions.xlsx', index=False)
-    reasons_df.to_excel(f'outputs/{model_name}_reasons.xlsx', index=False)
-
-    w_recall, w_precision, w_f1, m_f1 = eval_decisions(decisions_df, 'predictions', 'gold')
-
-    with open(f'decision_stats.tsv', 'a') as f:
-        f.write(
-            f'{model_name}\t{w_recall}\t{w_precision}\t{w_f1}\t{m_f1}\n')
+    reasons_df.to_excel(f'outputs/decision_reasons.xlsx', sheet_name=f"{model_name}", index=False)
 
 
 if __name__ == '__main__':
@@ -92,6 +132,6 @@ if __name__ == '__main__':
         description='''judgement prediction in UKSC cases''')
     parser.add_argument('--model_name', type=str, required=True, help='model_name')
     parser.add_argument('--visible_cuda_devices', type=str, default="0,1,2", required=False, help='model_name')
-    # parser.add_argument('--batch_size', type=int, required=False, default=8, help='batch_size')
+    parser.add_argument('--batch_size', type=int, required=False, default=8, help='batch_size')
     args = parser.parse_args()
     run(args)
