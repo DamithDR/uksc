7eab08b3b37268aa0a703ebcdbbd959bca2d808f
Damith Premasiri <damithpremasiri@gmail.com>
2025-02-10 17:34:18 +0530

    change truncation

--- experiments/llama3_extended.py
+++ experiments/llama3_extended.py
@@ -124,7 +124,7 @@ if __name__ == '__main__':
     parser.add_argument('--model_name', type=str, required=False, default='local_models/Meta-Llama-3.1-8B-Instruct',
                         help='model_name')
     parser.add_argument('--visible_cuda_devices', type=str, default="0,1,2", required=False, help='model_name')
-    parser.add_argument('--batch_size', type=int, required=False, default=4, help='batch_size')
+    parser.add_argument('--batch_size', type=int, required=False, default=8, help='batch_size')
     parser.add_argument('--run_mode', type=str, required=True, help='mode of prompt')
     args = parser.parse_args()
     run(args)
