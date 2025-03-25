d0c8a61e1933a16fac091a39ffda19bb594d4571
Damith Premasiri <damithpremasiri@gmail.com>
2025-03-25 11:37:06 +0000

    single run update

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ export HF_HOME="/mnt/data/dolamull/hf_cache"
 
 echo "Hugging Face cache directory set to: $HF_HOME"
 
-python -m experiments.llama3 --input_column judgment --model_name meta-llama/Meta-Llama-3.1-8B-Instruct --visible_cuda_devices 0,1,2
+python -m experiments.llama3 --input_column judgment --model_name meta-llama/Meta-Llama-3.1-8B-Instruct --visible_cuda_devices 0,1,2 --batch_size 4
