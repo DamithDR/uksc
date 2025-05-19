c6af7bb525fc2765f4b05a59e6d8af5892ef4ee1
Damith Premasiri <damithpremasiri@gmail.com>
2025-05-19 22:26:03 +0100

    longt5

--- experiments/longT5.py
+++ experiments/longT5.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
     MODEL_NAME = "google/long-t5-tglobal-base"
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
     MAX_INPUT_LENGTH = 4096
-    MAX_TARGET_LENGTH = 2048
+    MAX_TARGET_LENGTH = 1024
 
     train_df = pd.read_excel('data/historic/historic_data_with_reason.xlsx', sheet_name='data')
     train_df['prompt'] = train_df.apply(
