94cc323e587ee70e1b0752fdd9cfca831a788737
Damith Premasiri <damithpremasiri@gmail.com>
2025-05-19 22:28:42 +0100

    longt5

--- experiments/longT5.py
+++ experiments/longT5.py
@@ -13,7 +13,7 @@ def preprocess(example):
         example['input'],
         truncation=True,
         padding="max_length",
-        max_length=8192
+        max_length=MAX_INPUT_LENGTH
     )
 
     # Tokenize output (reasoning)
@@ -22,7 +22,7 @@ def preprocess(example):
             example['output'],
             truncation=True,
             padding="max_length",
-            max_length=2048
+            max_length=MAX_TARGET_LENGTH
         )
 
     model_input["labels"] = label["input_ids"]
@@ -49,8 +49,8 @@ if __name__ == '__main__':
 
     MODEL_NAME = "google/long-t5-tglobal-base"
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-    MAX_INPUT_LENGTH = 2048
-    MAX_TARGET_LENGTH = 512
+    MAX_INPUT_LENGTH = 4096
+    MAX_TARGET_LENGTH = 1048
 
     train_df = pd.read_excel('data/historic/historic_data_with_reason.xlsx', sheet_name='data')
     train_df['prompt'] = train_df.apply(
